1a4c243c76b31c7f4da05b7abe18cdd162ee6a6f
FlorentEstur <warcr@DESKTOP-3VUL9G4.home>
2022-11-02 14:15:20 +0100

    Changement getreference par find

--- src/main/java/com/inti/service/EtudiantServiceImpl.java
+++ src/main/java/com/inti/service/EtudiantServiceImpl.java
@@ -1,5 +1,7 @@
 package com.inti.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +17,7 @@ public class EtudiantServiceImpl implements IEtudiantService {
 	@Override
 	public Etudiant getEtudiant(int id) {
 
-		return ier.getReferenceById(id);
+		return ier.findById(id).get();
 	}
 
 }
